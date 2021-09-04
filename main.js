8e77153f121e0f3c6b5ee9a939f0ea787e6cc394
TA2k <tombox2020@gmail.com>
2021-09-04 20:29:19 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,365 @@
+"use strict";
+
+/*
+ * Created with @iobroker/create-adapter v1.34.1
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require("@iobroker/adapter-core");
+const axios = require("axios");
+const Json2iob = require("./lib/json2iob");
+const qs = require("qs");
+// Load your modules here, e.g.:
+// const fs = require("fs");
+
+class Ford extends utils.Adapter {
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    constructor(options) {
+        super({
+            ...options,
+            name: "ford",
+        });
+        this.on("ready", this.onReady.bind(this));
+        this.on("stateChange", this.onStateChange.bind(this));
+        this.on("unload", this.onUnload.bind(this));
+    }
+
+    /**
+     * Is called when databases are connected and adapter received configuration.
+     */
+    async onReady() {
+        // Reset the connection indicator during startup
+        this.setState("info.connection", false, true);
+        if (this.config.interval < 0.5) {
+            this.log.info("Set interval to minimum 0.5");
+            this.config.interval = 0.5;
+        }
+        this.requestClient = axios.create();
+        this.updateInterval = null;
+        this.reLoginTimeout = null;
+        this.refreshTokenTimeout = null;
+        this.json2iob = new Json2iob(this);
+        this.vinArray = [];
+        this.session = {};
+
+        this.subscribeStates("*");
+
+        await this.login();
+        if (this.session.access_token) {
+            await this.getVehicles();
+            await this.updateVehicles();
+            this.updateInterval = setInterval(async () => {
+                await this.updateVehicles();
+            }, this.config.interval * 60 * 1000);
+            this.refreshTokenInterval = setInterval(() => {
+                this.refreshToken();
+            }, this.session.expires_in * 1000);
+        }
+    }
+    async login() {
+        const headers = {
+            accept: "*/*",
+            "content-type": "application/x-www-form-urlencoded",
+            "user-agent": "FordPass/5 CFNetwork/1240.0.4 Darwin/20.6.0",
+            "accept-language": "de-de",
+            authorization: "Basic ZWFpLWNsaWVudDo=",
+        };
+        const data = {
+            client_id: "9fb503e0-715b-47e8-adfd-ad4b7770f73b",
+            username: this.config.username,
+            password: this.config.password,
+            grant_type: "password",
+        };
+        const authCode = await this.requestClient({
+            method: "post",
+            url: "https://sso.ci.ford.com/oidc/endpoint/default/token",
+            headers: headers,
+            data: qs.stringify(data),
+        })
+            .then((res) => {
+                this.log.debug(JSON.stringify(res.data));
+                return res.data.access_token;
+            })
+            .catch((error) => {
+                this.log.error(error);
+                if (error.response) {
+                    this.log.error(JSON.stringify(error.response.data));
+                }
+            });
+        if (!authCode) {
+            return;
+        }
+
+        await this.requestClient({
+            method: "put",
+            url: "https://api.mps.ford.com/api/oauth2/v1/token",
+
+            headers: {
+                accept: "*/*",
+                "content-type": "application/json",
+                "application-id": "1E8C7794-FF5F-49BC-9596-A1E0C86C5B19",
+                "user-agent": "FordPass/5 CFNetwork/1240.0.4 Darwin/20.6.0",
+                "accept-language": "de-de",
+            },
+            data: { code: authCode },
+        })
+            .then((res) => {
+                this.log.debug(JSON.stringify(res.data));
+                this.session = res.data;
+                this.setState("info.connection", true, true);
+                this.log.info("Login successful");
+                return res.data;
+            })
+            .catch((error) => {
+                this.log.error(error);
+                if (error.response) {
+                    this.log.error(JSON.stringify(error.response.data));
+                }
+            });
+    }
+    async getVehicles() {
+        const headers = {
+            "content-type": "application/json",
+            "application-id": "1E8C7794-FF5F-49BC-9596-A1E0C86C5B19",
+            accept: "*/*",
+            "auth-token": this.session.access_token,
+            locale: "DE-DE",
+            "accept-language": "de-de",
+            countrycode: "DEU",
+            "user-agent": "FordPass/5 CFNetwork/1240.0.4 Darwin/20.6.0",
+        };
+        await this.requestClient({
+            method: "post",
+            url: "https://api.mps.ford.com/api/expdashboard/v1/details/",
+            headers: headers,
+            data: JSON.stringify({
+                dashboardRefreshRequest: "All",
+            }),
+        })
+            .then(async (res) => {
+                this.log.debug(JSON.stringify(res.data));
+                for (const vehicle of res.data.userVehicles.vehicleDetails) {
+                    this.vinArray.push(vehicle.vin);
+                    await this.setObjectNotExistsAsync(vehicle.vin, {
+                        type: "device",
+                        common: {
+                            name: vehicle.model,
+                        },
+                        native: {},
+                    });
+                    await this.setObjectNotExistsAsync(vehicle.vin + ".remote", {
+                        type: "channel",
+                        common: {
+                            name: "Remote Controls",
+                        },
+                        native: {},
+                    });
+                    await this.setObjectNotExistsAsync(vehicle.vin + ".general", {
+                        type: "channel",
+                        common: {
+                            name: "General Car Information",
+                        },
+                        native: {},
+                    });
+
+                    const remoteArray = [{ command: "engine/start" }, { command: "doors/lock" }];
+                    remoteArray.forEach((remote) => {
+                        this.setObjectNotExists(vehicle.vin + ".remote." + remote.command, {
+                            type: "state",
+                            common: {
+                                name: remote.name || "",
+                                type: remote.type || "boolean",
+                                role: remote.role || "boolean",
+                                write: true,
+                                read: true,
+                            },
+                            native: {},
+                        });
+                    });
+                    this.json2iob.parse(vehicle.vin + ".general", vehicle);
+                }
+            })
+            .catch((error) => {
+                this.log.error(error);
+                error.response && this.log.error(JSON.stringify(error.response.data));
+            });
+    }
+
+    async updateVehicles() {
+        const statusArray = [{ path: "status", url: "https://api.mps.ford.com/api/vehicles/v4/$vin/status?lrdt=01-01-1970+00%3A00%3A00", desc: "Current status of the car" }];
+
+        const headers = {
+            "content-type": "application/json",
+            "application-id": "1E8C7794-FF5F-49BC-9596-A1E0C86C5B19",
+            accept: "*/*",
+            "auth-token": this.session.access_token,
+            locale: "DE-DE",
+            "accept-language": "de-de",
+            countrycode: "DEU",
+            "user-agent": "FordPass/5 CFNetwork/1240.0.4 Darwin/20.6.0",
+        };
+        this.vinArray.forEach((vin) => {
+            statusArray.forEach(async (element) => {
+                const url = element.url.replace("$vin", vin);
+
+                await this.requestClient({
+                    method: "get",
+                    url: url,
+                    headers: headers,
+                })
+                    .then((res) => {
+                        this.log.debug(JSON.stringify(res.data));
+                        if (!res.data) {
+                            return;
+                        }
+                        let data = res.data;
+                        const keys = Object.keys(res.data);
+                        if (keys.length === 1) {
+                            data = res.data[keys[0]];
+                        }
+                        const forceIndex = null;
+                        const preferedArrayName = null;
+
+                        this.json2iob.parse(vin + "." + element.path, data, { forceIndex: forceIndex, preferedArrayName: preferedArrayName, channelName: element.desc });
+                    })
+                    .catch((error) => {
+                        if (error.response && error.response.status === 401) {
+                            error.response && this.log.debug(JSON.stringify(error.response.data));
+                            this.log.info(element.path + " receive 401 error. Refresh Token in 30 seconds");
+                            clearTimeout(this.refreshTokenTimeout);
+                            this.refreshTokenTimeout = setTimeout(() => {
+                                this.refreshToken();
+                            }, 1000 * 30);
+
+                            return;
+                        }
+
+                        this.log.error(url);
+                        this.log.error(error);
+                        error.response && this.log.error(JSON.stringify(error.response.data));
+                    });
+            });
+        });
+    }
+
+    async refreshToken() {
+        await this.requestClient({
+            method: "put",
+            url: "https://api.mps.ford.com/api/oauth2/v1/refresh",
+
+            headers: {
+                accept: "*/*",
+                "content-type": "application/json",
+                "application-id": "1E8C7794-FF5F-49BC-9596-A1E0C86C5B19",
+                "user-agent": "FordPass/5 CFNetwork/1240.0.4 Darwin/20.6.0",
+                "accept-language": "de-de",
+            },
+            data: "refresh_token=" + this.session.refresh_token,
+        })
+            .then((res) => {
+                this.log.debug(JSON.stringify(res.data));
+                this.session = res.data;
+                this.setState("info.connection", true, true);
+                return res.data;
+            })
+            .catch((error) => {
+                this.log.error("refresh token failed");
+                this.log.error(error);
+                error.response && this.log.error(JSON.stringify(error.response.data));
+                this.log.error("Start relogin in 1min");
+                this.reLoginTimeout = setTimeout(() => {
+                    this.login();
+                }, 1000 * 60 * 1);
+            });
+    }
+
+    /**
+     * Is called when adapter shuts down - callback has to be called under any circumstances!
+     * @param {() => void} callback
+     */
+    onUnload(callback) {
+        try {
+            clearTimeout(this.refreshTimeout);
+            clearTimeout(this.reLoginTimeout);
+            clearTimeout(this.refreshTokenTimeout);
+            clearInterval(this.updateInterval);
+            clearInterval(this.refreshTokenInterval);
+            callback();
+        } catch (e) {
+            callback();
+        }
+    }
+
+    /**
+     * Is called if a subscribed state changes
+     * @param {string} id
+     * @param {ioBroker.State | null | undefined} state
+     */
+    async onStateChange(id, state) {
+        if (state) {
+            if (!state.ack) {
+                const vin = id.split(".")[2];
+
+                const command = id.split(".")[4];
+
+                const headers = {
+                    "content-type": "application/json",
+                    "application-id": "1E8C7794-FF5F-49BC-9596-A1E0C86C5B19",
+                    accept: "*/*",
+                    "auth-token": this.session.access_token,
+                    locale: "DE-DE",
+                    "accept-language": "de-de",
+                    countrycode: "DEU",
+                    "user-agent": "FordPass/5 CFNetwork/1240.0.4 Darwin/20.6.0",
+                };
+
+                const url = "https://api.mps.ford.com//api/vehicles/v2/" + vin + "/" + command;
+
+                await this.requestClient({
+                    method: state.val ? "put" : "delete",
+                    url: url,
+                    headers: headers,
+                })
+                    .then((res) => {
+                        this.log.debug(JSON.stringify(res.data));
+                        return res.data;
+                    })
+                    .catch((error) => {
+                        this.log.error(error);
+                        if (error.response) {
+                            this.log.error(JSON.stringify(error.response.data));
+                        }
+                    });
+                this.refreshTimeout = setTimeout(async () => {
+                    await this.updateVehicles();
+                }, 10 * 1000);
+            } else {
+                // const resultDict = { chargingStatus: "CHARGE_NOW", doorLockState: "DOOR_LOCK" };
+                // const idArray = id.split(".");
+                // const stateName = idArray[idArray.length - 1];
+                const vin = id.split(".")[2];
+                // if (resultDict[stateName]) {
+                //     let value = true;
+                //     if (!state.val || state.val === "INVALID" || state.val === "NOT_CHARGING" || state.val === "ERROR" || state.val === "UNLOCKED") {
+                //         value = false;
+                //     }
+                //     await this.setStateAsync(vin + ".remote." + resultDict[stateName], value, true);
+                // }
+            }
+        }
+    }
+}
+
+if (require.main !== module) {
+    // Export the constructor in compact mode
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    module.exports = (options) => new Ford(options);
+} else {
+    // otherwise start the instance directly
+    new Ford();
+}
