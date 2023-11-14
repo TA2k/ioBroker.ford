e0a2cf3c5ccae81e75cdf02f86ab932523984f4b
TA2k <tombox2020@gmail.com>
2023-11-14 12:30:37 +0100

    change v

--- main.js
+++ main.js
@@ -493,7 +493,7 @@ class Ford extends utils.Adapter {
         accept: "*/*",
         "content-type": "application/json",
         "application-id": this.appId,
-        "user-agent": "okhttp/4.9.2",
+        "user-agent": "okhttp/4.11.0",
         "accept-language": "de-de",
       },
       data: { refresh_token: this.session.refresh_token },
